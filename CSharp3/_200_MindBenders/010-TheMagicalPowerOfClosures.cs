d55f9014991680655b0929b2e2801d3f05615075
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-16 21:30:15 +0100

    Make mindbender more bendy

--- CSharp3/_200_MindBenders/010-TheMagicalPowerOfClosures.cs
+++ CSharp3/_200_MindBenders/010-TheMagicalPowerOfClosures.cs
@@ -88,33 +88,21 @@ namespace CSharp3._200_MindBenders
             Assert.That(Second(twoAndFour), Is.EqualTo(4));
         }
 
-        [Test]
-        public void TheSetOfPairsIsIsClosedOverTheOpertionOfMakePair()
-        {
-            var twoAndFourAndEightAndSixteen = MakePair(MakePair(2, 4), MakePair(8, 16)); //What is the type?
-            Assert.That(Second(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(16));
-            Assert.That(First(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(8));
-            Assert.That(First(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(2));
-            Assert.That(Second(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(4));
-        }
 
         #region Deep woodoo
-        //A hack created by Alonzo Church who lived before there was even the simplest computer...
 
-        static Func<Func<T, T, T>, T> MakePair<T>(T first, T second)
-        {
-            return (selector) => selector(first, second);
-        }
+        private readonly Func<int, int, Func<Func<int, int, int>, int>> MakePair = (first, second) => selector => selector(first, second);
+        private readonly Func<Func<Func<int, int, int>, int>, int> First = pair => pair((first, _) => first);
+        private readonly Func<Func<Func<int, int, int>, int>, int> Second = pair => pair((_, second) => second);
 
-        static T First<T>(Func<Func<T, T, T>, T> pair)
-        {
-            return pair((first, _) => first);
-        }
-
-        static T Second<T>(Func<Func<T, T, T>, T> pair)
-        {
-            return pair((_, second) => second);
-        }
+        //C# is not a bit noisy huh? Would you look at those type definitions!
+        
+        //Here's the same in F# with the twist that these pairs are once again 
+        //fully generic and closed over the set of all objects:
+        //
+        //  let MakePair =  fun first second -> fun selector -> selector first second
+        //  let First  = fun pair -> pair (fun first second -> first)
+        //  let Second = fun pair -> pair (fun first second -> second)
 
         #endregion
     }
