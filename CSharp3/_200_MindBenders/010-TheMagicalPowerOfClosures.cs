8d886eae222fb87485b1008550ad25e64469c8ef
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-16 17:12:31 +0100

    More magic with functions

--- CSharp3/_200_MindBenders/010-TheMagicalPowerOfClosures.cs
+++ CSharp3/_200_MindBenders/010-TheMagicalPowerOfClosures.cs
@@ -4,10 +4,9 @@ using NUnit.Framework;
 namespace CSharp3._200_MindBenders
 {
     /// <summary>
- 
     /// 
     /// A pair is something that can be created via MakePair 
-    /// examined via First and Second such that:
+    /// and examined via First and Second such that:
     /// First(MakePair(first, second)) = first
     /// and
     /// Second(MakePair(first, second)) = second
@@ -29,18 +28,17 @@ namespace CSharp3._200_MindBenders
         public void CallingSecondOnAPairReturnsTheSecondMemberOfThePair()
         {
             var twoAndFour = MakePair(2, 4); //What is the type?
-            Assert.That(Second(twoAndFour), Is.EqualTo(4));            
+            Assert.That(Second(twoAndFour), Is.EqualTo(4));
         }
 
         [Test]
         public void TheSetOfPairsIsIsClosedOverTheOpertionOfMakePair()
         {
-            var twoAndFourAndEightAndSixteen = MakePair(MakePair(2, 4), MakePair(8,16)); //What is the type?
+            var twoAndFourAndEightAndSixteen = MakePair(MakePair(2, 4), MakePair(8, 16)); //What is the type?
             Assert.That(Second(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(16));
             Assert.That(First(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(8));
             Assert.That(First(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(2));
-            Assert.That(Second(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(4));            
-
+            Assert.That(Second(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(4));
         }
 
         //Does it matter what the type of twoAndFour is if it satisfies the contract specified by the above rules?
@@ -48,21 +46,75 @@ namespace CSharp3._200_MindBenders
 
         #region Magic!
 
-        enum Index { First, Second }
-        static Func<Index, T> MakePair<T>(T first, T second)
+        private enum Index
+        {
+            First,
+            Second
+        }
+
+        private static Func<Index, T> MakePair<T>(T first, T second)
         {
             return x => x == Index.First ? first : second;
         }
 
-        static T First<T>(Func<Index, T> pair)
+
+        private static T First<T>(Func<Index, T> pair)
         {
             return pair(Index.First);
         }
 
-        static T Second<T>(Func<Index, T> pair)
+        private static T Second<T>(Func<Index, T> pair)
         {
             return pair(Index.Second);
-        } 
+        }
+
+        #endregion
+    }
+
+    [TestFixture]
+    public class GoingDeeper
+    {
+        [Test]
+        public void CallingFirstOnAPairReturnsTheFirstMemberOfThePair()
+        {
+            var twoAndFour = MakePair(2, 4); //What is the type?
+            Assert.That(First(twoAndFour), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CallingSecondOnAPairReturnsTheSecondMemberOfThePair()
+        {
+            var twoAndFour = MakePair(2, 4); //What is the type?
+            Assert.That(Second(twoAndFour), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TheSetOfPairsIsIsClosedOverTheOpertionOfMakePair()
+        {
+            var twoAndFourAndEightAndSixteen = MakePair(MakePair(2, 4), MakePair(8, 16)); //What is the type?
+            Assert.That(Second(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(16));
+            Assert.That(First(Second(twoAndFourAndEightAndSixteen)), Is.EqualTo(8));
+            Assert.That(First(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(2));
+            Assert.That(Second(First(twoAndFourAndEightAndSixteen)), Is.EqualTo(4));
+        }
+
+        #region Deep woodoo
+        //A hack created by Alonzo Church who lived before there was even the simplest computer...
+
+        static Func<Func<T, T, T>, T> MakePair<T>(T first, T second)
+        {
+            return (selector) => selector(first, second);
+        }
+
+        static T First<T>(Func<Func<T, T, T>, T> pair)
+        {
+            return pair((first, _) => first);
+        }
+
+        static T Second<T>(Func<Func<T, T, T>, T> pair)
+        {
+            return pair((_, second) => second);
+        }
 
         #endregion
     }
