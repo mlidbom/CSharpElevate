54ca7a667367c6ab3a904dccf795a2348dc97dd6
mlidbom <magnus.lidbom@gmail.com>
2010-01-16 00:24:02 +0100

    Improve comments.

--- CSharp3/_001_Intro/TheElephantInTheRoom.cs
+++ CSharp3/_001_Intro/TheElephantInTheRoom.cs
@@ -22,23 +22,22 @@ namespace CSharp3._001_Intro
     {
         #region The wrong way
 
-        private static long SizeOfDirectoryClassic(string directory)
-        {
-            long result = 0;
-            var subDirectorys = Directory.GetDirectories(directory);
-            foreach (var subDirectory in subDirectorys)
-            {
-                result += SizeOfDirectoryClassic(subDirectory);
-            }
+private static long SizeOfDirectoryClassic(string directory)
+{
+    long result = 0;
+    var subDirectories = Directory.GetDirectories(directory);
+    foreach (var subDirectory in subDirectories)
+    {
+        result += SizeOfDirectoryClassic(subDirectory);
+    }
 
-            var files = Directory.GetFiles(directory);
-            foreach (var file in files)
-            {
-                result += new FileInfo(file).Length;
-            }
-
-            return result;
-        }
+    var files = Directory.GetFiles(directory);
+    foreach (var file in files)
+    {
+        result += new FileInfo(file).Length;
+    }
+    return result;
+}
 
         #region And what's wrong with that?
 
@@ -52,7 +51,11 @@ namespace CSharp3._001_Intro
         //
         //violates: SRP, DRY and OCP
         //
-        //domain is implicit
+        //domain is implicit: string, array etc
+        //
+        //Multipurpose lines
+        //
+        //temporary variables
 
         #endregion
 
