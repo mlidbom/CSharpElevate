d7b61d3ea7c651983224ff64751d15f79a9c5b71
mlidbom <magnus.lidbom@gmail.com>
2010-01-17 17:04:48 +0100

    Get rid of cruft.

--- CSharp3/_080_Linq/_010_ExtensionMethods/010-WithExtentionMethods.cs
+++ CSharp3/_080_Linq/_010_ExtensionMethods/010-WithExtentionMethods.cs
@@ -1,11 +1,8 @@
 using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using CSharp3._050_ExtensionMethods;
-using CSharp3._090_PrinciplesViaSolid._020_UseAndCreateClosures;
-using CSharp3.Extensions.Hierarchies;
 using CSharp3.Util;
-using CSharp3.Util.Linq;
 using NUnit.Framework;
 
 namespace CSharp3._080_Linq._010_ExtensionMethods
@@ -32,118 +29,138 @@ namespace CSharp3._080_Linq._010_ExtensionMethods
     ///     
     /// </summary>
     [TestFixture]
-    public class WithExtentionMethods : NUnitTestBase
+    public class FundamentalOperators : NUnitTestBase
     {
-        private static readonly string ADirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private static IEnumerable<T> Seq<T>(params T[] elements)
+        {
+            return elements;
+        }
+
+        #region filtering
 
         [Test]
-        public void WhereFilters()
+        public void Where()
         {
-            var oddsIn1Through10 = 1.Through(10).Where(number => number%2 != 0);
-            Assert.That(oddsIn1Through10, Is.EqualTo(Seq.New(1,3,5,7,9)));
+            var oddsIn1Through10 = 1.Through(4).Where(number => number > 2); //3,4
+
+            oddsIn1Through10 = from number in 1.Through(4)
+                               where number > 2
+                               select number; //3,4
+            #region asserts
+
+            Assert.That(oddsIn1Through10, Is.EqualTo(Seq(3, 4)));
+            Assert.That(oddsIn1Through10, Is.EqualTo(Seq(3, 4)));
+
+            #endregion
         }
 
         [Test]
-        public void SelectTransforms()
+        public void Distinct()
         {
-            var doubleOf1Through4 = 1.Through(4).Select(me => me * 2);
-            Assert.That(doubleOf1Through4, Is.EqualTo(Seq.New(2,4,6,8)));
+            var distinct = Seq(1, 1, 2, 2).Distinct(); //1,2
+
+            #region asserts
+            Assert.That(distinct, Is.EqualTo(Seq(1, 2)));
+            #endregion
+        }
+
+        #endregion
+
+        #region projections
+
+        [Test]
+        public void SelectProjects()
+        {
+            var doubleOf1Through4 = 1.Through(4).Select(me => me*2);
+            Assert.That(doubleOf1Through4, Is.EqualTo(Seq(2, 4, 6, 8)));
+
+            doubleOf1Through4 = from number in 1.Through(4)
+                                select number*2;
+            Assert.That(doubleOf1Through4, Is.EqualTo(Seq(2, 4, 6, 8)));
         }
 
         [Test]
-        public void SumSums()
+        public void SelectManyProjectsAndFlattens()
         {
-            var sumOf1Through4 = 1.Through(4).Sum();
-            var sumOf1Through4Explicit = 1.Through(4).Sum(me => me);
+            var oneThrough4Grouped = Seq(Seq(1, 2), Seq(3, 4));
+            var oneThrough4Sequential = oneThrough4Grouped.SelectMany(me => me);
+            Assert.That(oneThrough4Sequential, Is.EqualTo(Seq(1, 2, 3, 4)));
 
-            Assert.That(sumOf1Through4, Is.EqualTo(10));
-            Assert.That(sumOf1Through4Explicit, Is.EqualTo(10));
+            oneThrough4Sequential = from URDU in oneThrough4Grouped
+                                    from child in URDU
+                                    select child;
+            Assert.That(oneThrough4Sequential, Is.EqualTo(Seq(1, 2, 3, 4)));
+        }
+
+        #endregion
+
+        #region aggregation
+
+        [Test]
+        public void BasicAggregationOperators()
+        {
+            var oneThrough4 = 1.Through(4);
+
+            var min = oneThrough4.Min(); //1                        
+            var max = oneThrough4.Max(); //4            
+            var sum = oneThrough4.Sum(); //10           
+            var average = oneThrough4.Average(); //2.5
+
+
+            var minDouble = oneThrough4.Min(num => num*2); //2
+            var maxDouble = oneThrough4.Max(num => num*2); //8
+            var sumDouble = oneThrough4.Sum(num => num*2); //20
+            var averageDouble = oneThrough4.Average(num => num*2); //5
+
+            //No comprehension parallels
+
+            #region asserts
+
+            Assert.That(min, Is.EqualTo(1));
+            Assert.That(max, Is.EqualTo(4));
+            Assert.That(sum, Is.EqualTo(10));
+            Assert.That(average, Is.EqualTo(2.5));
+
+            Assert.That(minDouble, Is.EqualTo(2));
+            Assert.That(maxDouble, Is.EqualTo(8));
+            Assert.That(sumDouble, Is.EqualTo(20));
+            Assert.That(averageDouble, Is.EqualTo(5.0));
+
+            #endregion
         }
 
         [Test]
         public void AggregateAggregatesAndTransforms()
         {
-            var oneThrough4AsStrings = 1.Through(4)
-                .Select(me => me.ToString())
-                .Aggregate((aggregate, file) => aggregate + "," + file);
+            var names = Seq("Calle", "Oscar");
 
-            Assert.That(oneThrough4AsStrings, Is.EqualTo("1,2,3,4"));
+            var namesCombined = names.Aggregate((aggregate, file) => aggregate + "," + file); //"Calle,Oscar"
 
+            Assert.That(namesCombined, Is.EqualTo("Calle,Oscar"));
 
-            var sumOf1Through4  = 1.Through(4)
-                .Aggregate(0, (sum, current) => sum + current);
-
-            Assert.That(sumOf1Through4, Is.EqualTo(10));
+            //No comprehension parallel
         }
 
-        [Test]
-        public void SelectManyFlattens()
-        {
-            var oneThrough4Grouped = Seq.New(Seq.New(1, 2), Seq.New(3, 4));
-            var oneThrough4Sequential = oneThrough4Grouped.SelectMany(me => me);
-            Assert.That(oneThrough4Sequential, Is.EqualTo(Seq.New(1,2,3,4)));
-        }
-
-        [Test]
-        public void DistinctRemovesDuplicates()
-        {
-            var oneThrough5WithDuplicates = Seq.New(1, 1, 2, 2, 3, 3, 4, 5);
-            var oneThrough5Unique = oneThrough5WithDuplicates.Distinct();
-            Assert.That(oneThrough5Unique, Is.EqualTo(Seq.New(1,2,3,4,5)));
-        }
+        #endregion
 
         [Test]
         public void ExceptFiltersOutAllElementsInTheArgumentSequence()
         {
-            var oneThrough5Except2Through4 = 1.Through(5).Except(2.Through(4));
-            Assert.That(oneThrough5Except2Through4, Is.EqualTo(Seq.New(1,5)));
+            Assert.That(
+                1.Through(5).Except(2.Through(4)),
+                Is.EqualTo(Seq(1, 5)));
         }
 
         [Test]
         public void GroupByGroups()
         {
-            Func<int, bool> isEven = me => me % 2 == 0;
+            Func<int, bool> isEven = me => me%2 == 0;
             var groupedByEvenUneven = 1.Through(10).GroupBy(isEven);
             var even = groupedByEvenUneven.Where(grouping => grouping.Key).Single();
             var odd = groupedByEvenUneven.Where(grouping => !grouping.Key).Single();
-            
-            Assert.That(even, Is.EqualTo(Seq.New(2, 4, 6, 8, 10)));
-            Assert.That(odd, Is.EqualTo(Seq.New(1, 3, 5, 7, 9)));
-        }
 
-        [Test]
-        public void RememberMe()
-        {
-            ADirectory.AsHierarchy(Directory.GetDirectories).FlattenHierarchy()
-                .SelectMany(dir => Directory.GetFiles(dir.Decorated))
-                .Sum(file => new FileInfo(file).Length)
-                .Do(Console.WriteLine);
-
-
-            //Verbose version
-
-            //Recursive tree walk. Finds all folders below and including "folder" 
-            //by recursively calling Directory.GetDirectories
-            //and yielding each one
-            var sizeOfFolder = ADirectory.AsHierarchy(Directory.GetDirectories).FlattenHierarchy()
-
-                //transforms the IEnumerable of folder names into an 
-                //IEnumerable of string[]with all the files in each folder
-                .Select(currentDirectory => Directory.GetFiles(currentDirectory.Decorated))
-
-                //flattens the IEnumerable of string[] into an IEnumerable of string
-                .SelectMany(files => files)
-
-                //Transforms the filepaths into FileInfo objects
-                .Select(file => new FileInfo(file)) //transformation 
-
-                //Transforms the FileInfo objects into longs containing
-                //their size
-                .Select(fileinfo => fileinfo.Length) //transformation
-
-                //Sums all the file sizes.
-                .Sum(); //aggregation.
-            Console.WriteLine(sizeOfFolder);
+            Assert.That(even, Is.EqualTo(Seq(2, 4, 6, 8, 10)));
+            Assert.That(odd, Is.EqualTo(Seq(1, 3, 5, 7, 9)));
         }
     }
 }
